1f10221f35fd58e221380e20703d44977a196cfb
bytarch <159407182+bytarch@users.noreply.github.com>
2024-11-24 19:05:24 -0600

    Update and rename fake_data_starter.sql to fake_data_starter.nxus

--- fake_data_starter.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO ADMIN (username, PASSWORD, email, created_at, updated_at) VALUES
-('adminUser1', 'securePassword123', 'admin1@example.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
-INSERT INTO visitee (NAME, unit_number, phone, email, created_at, PASSWORD) VALUES
-('John Doe', 'Unit 101', '555-1234', 'johndoe@example.com', CURRENT_TIMESTAMP, 'visitorPassword456');
